e373f02684ff75d665416275a7231c659451b38f
Shota Narisaka <shootacean@gmail.com>
2024-05-19 22:02:29 +0900

    react.dev tutorial plus ultra 2

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -10,8 +10,6 @@ function Square({value, onSquareClick}: {value: string | null, onSquareClick: ()
 }
 
 function Board({xIsNext, squares, onPlay}: {xIsNext: boolean, squares: Array<string | null>, onPlay: (squares: Array<string | null>) => void}) {
-    // const [squares, setSquares] = useState(Array(9).fill(null));
-
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
@@ -30,24 +28,18 @@ function Board({xIsNext, squares, onPlay}: {xIsNext: boolean, squares: Array<str
         }
         onPlay(nextSquares);
     }
+
     return (
         <>
-        <div className='mb-[10px]'>{status}</div>
-            <div className="clear-both content-[''] table">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-            </div>
-            <div className="clear-both content-[''] table">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-            </div>
-            <div className="clear-both content-[''] table">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-            </div>
+            <div className='mb-[10px]'>{status}</div>
+            {[1,2,3].map((row) => (
+                <div className="clear-both content-[''] table">
+                    {[1,2,3].map((col) => {
+                        const cell = (row - 1) * 3 + col - 1;
+                        return <Square value={squares[cell]} onSquareClick={() => handleClick(cell)}/>
+                    })}
+                </div>
+            ))}
         </>
     )
 }
